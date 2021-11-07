8f8ccacdf5e801b5ce9e7dcec734beecf19af997
Adrian <eadepto@hotmail.com>
2021-11-07 14:27:51 -0500

    productor consumidor

---
+++ 01-proyecto/productor.js
@@ -0,0 +1,25 @@
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['0:9092', '1:9093', '2:9094']
+})
+
+const producer = kafka.producer()
+
+async function init(){
+    await producer.connect()
+
+    await producer.send({
+        topic: 'topico_replicado',
+        messages: [
+        { value: 'Hola amigos de kafka 1' },
+        { value: 'Hola amigos de kafka 2' },
+        { value: 'Hola amigos de kafka 3' },
+        ],
+    })
+    
+    await producer.disconnect()
+}
+
+init().then().catch();
